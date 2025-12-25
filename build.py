d956b2c65f1928549ef5c790e01dc7e1dddcbd42
yokamoto5742-h <yokamoto5742@gmail.com>
2025-12-25 17:25:20 +0900

    1st commit

---
+++ build.py
@@ -0,0 +1,21 @@
+import subprocess
+
+from scripts.version_manager import update_version
+
+
+def build_executable():
+    new_version = update_version()
+    subprocess.run([
+        "pyinstaller",
+        "--name=app_name",
+        "--windowed",
+        "--add-data", "utils/config.ini:.",
+        "main.py"
+    ])
+
+    print(f"Executable built successfully. Version: {new_version}")
+    return new_version
+
+
+if __name__ == "__main__":
+    build_executable()
